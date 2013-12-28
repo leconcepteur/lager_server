8ab03f44bf3b025f704c50c2d08139eb169ad88b
Jonathan Dube <jdube@humanequation.co>
2013-12-28 09:42:57 -0500

    Added connection info

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var Pusher = require('pusher');
 
 var pusher = new Pusher({
-  appId: 'APP_ID',
-  key: 'APP_KEY',
-  secret: 'APP_SECRET'
+  appId: '62545',
+  key: '6186696886e0a79c983c',
+  secret: '9e35216cedc6623651dc'
 });
 
 pusher.trigger('my-channel', 'my-event', {"message": "hello world"});
